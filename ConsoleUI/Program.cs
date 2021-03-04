d0ffd6ed41b3fe6a14a88a3d7d4ab9c3bd63c2be
Emre <emredalc@gmail.com>
2021-03-04 22:33:53 +0300

    8. Gün Ödev - ConsoleUI Denemeleri

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,6 +2,7 @@
 using Bussiness.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 
 namespace ConsoleUI
 {
@@ -9,6 +10,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----Vehicles----");
             CarManager carManager = new CarManager(new EfCarDal());
 
             foreach (var car in carManager.GetAll())
@@ -16,6 +18,42 @@ namespace ConsoleUI
                 Console.WriteLine(car.CarName);
                 Console.WriteLine(car.DailyPrice);
                 Console.WriteLine(car.ModelYear);
+            }
+
+            AddCarTest();
+            UpdateCarTest();
+            DeleteCarTest();
+        }
+
+        public static void AddCarTest() 
+        {
+            var carManager = new CarManager(new EfCarDal());
+            carManager.Add(new Car { BrandId = 2, ColorId = 1, CarName = "BMW 316i", DailyPrice = 400, Descriptions = "Manual", ModelYear = 2019 });
+            Console.WriteLine("---Vehicles---");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarName);
+            }
+        }
+
+        public static void UpdateCarTest() 
+        {
+            var carManager = new CarManager(new EfCarDal());
+            carManager.Update(new Car { Id = 1005, BrandId = 2, ColorId = 1, CarName = "BMW 316_", DailyPrice = 400, Descriptions = "Manual", ModelYear = 2019 });
+            Console.WriteLine("---Vehicles---");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarName);
+            }
+        }
+        public static void DeleteCarTest() 
+        {
+            var carManager = new CarManager(new EfCarDal());
+            carManager.Delete(new Car { Id = 1005, BrandId = 2, ColorId = 1, CarName = "BMW 316_", DailyPrice = 400, Descriptions = "Manual", ModelYear = 2019 });
+            Console.WriteLine("---Vehicles---");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarName);
             }
         }
     }
